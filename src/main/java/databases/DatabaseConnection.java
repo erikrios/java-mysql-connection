6d021a2d0ea198e357acc11c7341e187420846d3
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-25 22:02:43 +0700

    Add a method to add data

--- src/main/java/databases/DatabaseConnection.java
+++ src/main/java/databases/DatabaseConnection.java
@@ -2,7 +2,10 @@ package databases;
 
 import models.FavoriteFood;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class DatabaseConnection {
 
