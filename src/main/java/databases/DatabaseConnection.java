cfdc80673b9ea186cbfde7c94f197d9ac5d8032b
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-25 21:41:29 +0700

    Add some database properties

--- src/main/java/databases/DatabaseConnection.java
+++ src/main/java/databases/DatabaseConnection.java
@@ -7,5 +7,18 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 
+    private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    private final String DATABASE_NAME = "foods";
+    private final String TABLE_NAME = "favorite_foods";
+    private final String DATABASE_URL = "jdbc:mysql://localhost/" + DATABASE_NAME;
+    private final String MYSQL_USERNAME = "erikrios";
+    private final String MYSQL_PASSWORD = "";
 
+    private Connection connection;
+    private Statement statement;
+    private ResultSet resultSet;
+
+    private DatabaseConnection() {
+
+    }
 }
