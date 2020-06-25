8c786f381039a146b1e02c77ee6245d3ffe94403
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-25 22:02:27 +0700

    Add a method to add data

--- src/main/java/databases/DatabaseConnection.java
+++ src/main/java/databases/DatabaseConnection.java
@@ -1,9 +1,8 @@
 package databases;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.sql.ResultSet;
-import java.sql.DriverManager;
+import models.FavoriteFood;
+
+import java.sql.*;
 
 public class DatabaseConnection {
 
@@ -11,21 +10,40 @@ public class DatabaseConnection {
     private Statement statement;
     private ResultSet resultSet;
 
+    final String DATABASE_NAME = "foods";
+    final String TABLE_NAME = "favorite_foods";
+    final String COLUMN_NAME = "name";
+    final String COLUMN_PRICE = "price";
+    final String USERNAME = "erikrios";
+    final String PASSWORD = "";
+    final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    final String DATABASE_URL = "jdbc:mysql://localhost/" + DATABASE_NAME;
+
     private DatabaseConnection() {
         createConnection();
     }
 
     private void createConnection() {
-        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-        final String DATABASE_NAME = "foods";
-        final String TABLE_NAME = "favorite_foods";
-        final String DATABASE_URL = "jdbc:mysql://localhost/" + DATABASE_NAME;
-        final String USERNAME = "erikrios";
-        final String PASSWORD = "";
 
         try {
             Class.forName(JDBC_DRIVER);
             connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void add(FavoriteFood favoriteFood) {
+        try {
+            statement = connection.createStatement();
+
+            String foodName = favoriteFood.getName();
+            int foodPrice = favoriteFood.getPrice();
+
+            String format = "INSERT INTO %s (%s, %s) VALUES (%s, %d);";
+            String sql = String.format(format, TABLE_NAME, COLUMN_NAME, COLUMN_PRICE, foodName, foodPrice);
+
+            statement.execute(sql);
         } catch (Exception e) {
             e.printStackTrace();
         }
