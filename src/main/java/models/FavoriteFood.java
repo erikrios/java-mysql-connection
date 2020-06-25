2bf1eee1420a0d35741f7948d238752e2c9a7ecd
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-25 21:32:47 +0700

    Add class constructors

--- src/main/java/models/FavoriteFood.java
+++ src/main/java/models/FavoriteFood.java
@@ -5,4 +5,10 @@ public class FavoriteFood {
     private int id;
     private String name;
     private int price;
+
+    public FavoriteFood(int id, String name, int price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 }
