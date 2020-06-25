c82fdc1baed4fcb825f2811758c84b5369cbfe3a
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-06-25 21:48:22 +0700

    Create a method to connect to database

--- src/main/java/databases/DatabaseConnection.java
+++ src/main/java/databases/DatabaseConnection.java
@@ -7,18 +7,27 @@ import java.sql.DriverManager;
 
 public class DatabaseConnection {
 
-    private final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-    private final String DATABASE_NAME = "foods";
-    private final String TABLE_NAME = "favorite_foods";
-    private final String DATABASE_URL = "jdbc:mysql://localhost/" + DATABASE_NAME;
-    private final String MYSQL_USERNAME = "erikrios";
-    private final String MYSQL_PASSWORD = "";
-
     private Connection connection;
     private Statement statement;
     private ResultSet resultSet;
 
     private DatabaseConnection() {
+        createConnection();
+    }
 
+    private void createConnection() {
+        final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+        final String DATABASE_NAME = "foods";
+        final String TABLE_NAME = "favorite_foods";
+        final String DATABASE_URL = "jdbc:mysql://localhost/" + DATABASE_NAME;
+        final String USERNAME = "erikrios";
+        final String PASSWORD = "";
+
+        try {
+            Class.forName(JDBC_DRIVER);
+            connection = DriverManager.getConnection(DATABASE_URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
